1e160a7a6e77633b96cae5b64f4cb87f4cf8ded7
kokoKeremidata <148246518+kokoKeremidata@users.noreply.github.com>
2024-11-30 12:23:03 +0200

    Update guessingGameGUI.java

--- GUI/guessingGameGUI.java
+++ GUI/guessingGameGUI.java
@@ -6,17 +6,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class guessingGameGUI {
+    private static int randomNumber;
+
     public static void main(String[] args) {
         //UI
+        //Panel 1
         JFrame frame = new JFrame("Guessing game");
         JPanel dialogue = new JPanel(new FlowLayout());
         JLabel dialogueText = new JLabel("Welcome to the guessing game!\nGuess a number between 1 and 100");
-
+        //Panel 2
         JPanel inputP = new JPanel();
         JTextField userInput = new JTextField(5);
         JButton guessB = new JButton("Guess");
-
-        frame.setLayout(new GridLayout(2,1));
+        //Panel 3
+        JPanel control = new JPanel();
+        JButton quit = new JButton("Quit");
+        JButton newGame = new JButton("New Game");
+        //Frame
+        frame.setLayout(new GridLayout(3,1));
         frame.setSize(500,500);
 
         frame.add(dialogue);
@@ -24,97 +31,68 @@ public class guessingGameGUI {
         frame.add(inputP);
         inputP.add(userInput);
         inputP.add(guessB);
-        
+        frame.add(control);
+        control.add(quit);
+        control.add(newGame);
+
+        setStyleL(dialogueText);
+        setStyleT(userInput);
+        setStyleB(guessB);
+        setStyleB(quit);
+        setStyleB(newGame);
+
         frame.setVisible(true);
+
         //CODE
-        int attempts = 0;
-        int guess = 0;
-        boolean guessed = false;
-        System.out.println("Welcome to the guessing game! Guess a number between 1 and 100!");
-
-        //generate num
         Random rand = new Random();
-        int randomNumber = rand.nextInt(1,101);
+        randomNumber = rand.nextInt(1,101);
 
-        Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("Take a guess!");
         dialogueText.setText("Take a guess!");
 
-        int num1 = Integer.parseInt(userInput.getText());
-
-        while(guessed==false){
-            if(sc.hasNextInt()){
-                guess = sc.nextInt();
-                if(guess==randomNumber){
-                    System.out.println("Congratulations! You guessed the number!");
-                    dialogueText.setText("Congratulations! You guessed the number!");
-                    guessed=true;
-                    System.exit(0);
-                }else if(guess<randomNumber){
-                    System.out.println("Guess higher!");
-                    dialogueText.setText("Guess higher!");
-                }else if(guess>randomNumber){
-                    System.out.println("Guess lower!");
-                    dialogueText.setText("Guess lower!");
+        guessB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                boolean guessed = false;
+                int attempts = 0;
+                try {
+                    int num1 = Integer.parseInt(userInput.getText());
+                    if(num1 == randomNumber){
+                        dialogueText.setText("Congratulations! You guessed the number!");
+                        guessed=true;
+                    }else if (num1>100 || num1<1) {
+                        dialogueText.setText("Please enter a number between 1 and 100!");
+                    }else if(num1<randomNumber){
+                        dialogueText.setText("Guess higher!");
+                    }else if(num1>randomNumber){
+                        dialogueText.setText("Guess lower!");
+                    }
+                    attempts++;
                 }
-                attempts++;
-            }else if(sc.hasNextDouble()){
-                double number = userInput.nextDouble();
-                System.out.println("Enter a whole number between 1 and 100!");
-                dialogueText.setText("Enter a whole number between 1 and 100!");
-            }else if(sc.hasNextLong()){
-                long number = sc.nextLong();
-                dialogueText.setText("Enter a number between 1 and 100!");
-            }else{
-                String input = sc.next();
-                if (input.equals("Quit")||input.equals("quit")){
-                    dialogueText.setText("The number was " + randomNumber + "\nAttempts: " + attempts);
-                    System.exit(0);
-                }else{
-                    dialogueText.setText("Enter a valid statement");
+                catch (NumberFormatException a) {
+                    dialogueText.setText("Invalid input");
                 }
             }
-        }
+        });
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        newGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                randomNumber = rand.nextInt(1,101);
+                dialogueText.setText("Take a guess!");
+                userInput.setText("");
+            }
+        });
 
     }
-    /*public static void calculate(JTextField guess, boolean guessed, int randomNumber){
-       double num1 = Double.parseDouble(numberGuessed.getText());
-
-        while(guessed==false){
-            if(sc.hasNextInt()){
-                guess = sc.nextInt();
-                if(guess==randomNumber){
-                    System.out.println("Congratulations! You guessed the number!");
-                    dialogueText.setText("Congratulations! You guessed the number!");
-                    guessed=true;
-                    System.exit(0);
-                }else if(guess<randomNumber){
-                    System.out.println("Guess higher!");
-                    dialogueText.setText("Guess higher!");
-                }else if(guess>randomNumber){
-                    System.out.println("Guess lower!");
-                    dialogueText.setText("Guess lower!");
-                }
-                attempts++;
-            }else if(sc.hasNextDouble()){
-                double number = sc.nextDouble();
-                System.out.println("Enter a whole number between 1 and 100!");
-                dialogueText.setText("Enter a whole number between 1 and 100!");
-            }else if(sc.hasNextLong()){
-                long number = sc.nextLong();
-                dialogueText.setText("Enter a number between 1 and 100!");
-            }else{
-                String input = sc.next();
-                if (input.equals("Quit")||input.equals("quit")){
-                    dialogueText.setText("The number was " + randomNumber + "\nAttempts: " + attempts);
-                    System.exit(0);
-                }else{
-                    dialogueText.setText("Enter a valid statement");
-                }
-            }
-        }
-       }
-    }*/
+    public static void setStyleB(JButton button){
+        button.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+    }
+    public static void setStyleL(JLabel button){
+        button.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+    }
+    public static void setStyleT(JTextField button){
+        button.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+    }
 }
