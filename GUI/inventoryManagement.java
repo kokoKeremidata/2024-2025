99eaed8e84f4093b57d0610bebb64fe234bd694c
kokoKeremidata <148246518+kokoKeremidata@users.noreply.github.com>
2025-01-09 19:34:59 +0200

    Update inventoryManagement.java

--- GUI/inventoryManagement.java
+++ GUI/inventoryManagement.java
@@ -4,12 +4,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args){
+        //Create inventory object to access
         Inventory inventory = new Inventory();
-        //frame
+
+        //Create frame
         JFrame frame = new JFrame("Inventory Management");
         frame.setLayout(new GridLayout(2,1));
-        frame.setSize(600,600);
-        //split into 3 panels using 4 panels
+        frame.setSize(600,400);
+
+        //Make 2 panels and split the top one into two (input on the left, output on the right)
         JPanel panel1 = new JPanel(new FlowLayout());
         panel1.setLayout(new GridLayout(1,2));
         JPanel panel2 = new JPanel(new FlowLayout());
@@ -19,19 +22,28 @@ public class Main {
         frame.add(panel2);
         panel1.add(panel3);
         panel1.add(panel4);
-        //textfields
-        JTextField eName = new JTextField(15);
-        JTextField ePrice = new JTextField(15);
-        JTextField eQuantity = new JTextField(15);
+
+        JLabel label = new JLabel("Name:");
+        JLabel label2 = new JLabel("Price:");
+        JLabel label3 = new JLabel("Quantity:");
+        //Create textfields for input
+        JTextField eName = new JTextField(20);
+        JTextField ePrice = new JTextField(20);
+        JTextField eQuantity = new JTextField(20);
+        panel3.add(label);
         panel3.add(eName);
+        panel3.add(label2);
         panel3.add(ePrice);
+        panel3.add(label3);
         panel3.add(eQuantity);
-        //display info
+
+        //Create area to display info
         JTextArea displayDetails = new JTextArea();
         JTextArea output = new JTextArea();
         panel4.add(displayDetails);
         panel4.add(output);
-        //click click
+
+        //Create buttons
         JButton addProduct = new JButton("Add Product");
         JButton updateProduct = new JButton("Update Product");
         JButton display = new JButton("Display Products");
@@ -39,9 +51,10 @@ public class Main {
         panel2.add(updateProduct);
         panel2.add(display);
 
-        //
+        //Make buttons work
         addProduct.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                //Make sure there are values to take
                 if(eName.getText().equals("")){
                     output.setText("Invalid product");
                 }
@@ -51,16 +64,18 @@ public class Main {
                 if(eQuantity.getText().equals("")){
                     output.setText("Invalid product");
                 }
+                //If the parses fail, stop the program from crashing and just output an error
                 try{
                     output.setText( inventory.addProduct(new Product(eName.getText(),Double.parseDouble(ePrice.getText()),Integer.parseInt(eQuantity.getText()))));
-
                 }catch(NumberFormatException nfe){
-                    output.setText("Invalid product");
+                    output.setText("Invalid price or quantity");
                 }
             }
         });
+
         updateProduct.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                //Make sure there are values to take
                 if(eName.getText().equals("")){
                     output.setText("Invalid product");
                 }
@@ -74,7 +89,7 @@ public class Main {
                     inventory.updateProduct(eName.getText(),Double.parseDouble(ePrice.getText()),Integer.parseInt(eQuantity.getText()));
                     output.setText("Product Updated");
                 }catch(NumberFormatException nfe){
-                    output.setText("Invalid product");
+                    output.setText("Please choose an existing product\nor enter valid price and quantity");
                 }
             }
         });
@@ -92,29 +107,35 @@ class Product{
     private String name;
     private double price;
     private int quantity;
+    //Constructor
     public Product(String name, double price, int quantity){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
-    //getters
+
+    //Getters
     public String getName(){
         return name;
     }
+
     public double getPrice(){
         return price;
     }
+
     public int getQuantity(){
         return quantity;
     }
 
-    //setters
+    //Setters
     public void setName(String name){
         this.name = name;
     }
+
     public void setPrice(double price){
         this.price = price;
     }
+
     public void setQuantity(int quantity){
         this.quantity = quantity;
     }
@@ -122,38 +143,51 @@ class Product{
 class Inventory{
     private Product[] products;
     private int proCount;
+
     public Inventory(){
         products = new Product[3];
         proCount = 0;
     }
+
     public String addProduct(Product product){
-        for(int i = 0; i < products.length; i++){
-            if(products[i] == null){
-                products[i] = product;
-                return "Product Added";
-            }else if (products[i]!=null && i == products.length-1){
-                return "Inventory full";
+        if(product.getPrice()>0 && product.getQuantity()>0) { //check for negative values
+            //search for empty slot and add product
+            for (int i = 0; i < products.length; i++) {
+                if (products[i] == null) {
+                    products[i] = product;
+                    return "Product Added";
+                } else if (products[i] != null && i == products.length - 1) {
+                    return "Inventory full";
+                }
+            }
+            //stops the same product from being added 3 times from one button press (somehow)
+            if (proCount < products.length) {
+                products[proCount] = product;
+                proCount++;
+                return "Product added";
+            } else {
+                return "Inventory is full";
+            }
+        }else{
+            return "Please enter positive values";
+        }
+    }
+
+    public void updateProduct(String name, double price, int quantity){
+        //search for an existing product with the same name, can only change price and quantity intentionally
+        if(price > 0 && quantity>=0) {
+            for (int i = 0; i < products.length; i++) {
+                if (products[i] != null && products[i].getName().equals(name)) { // Corrected condition
+                    products[i].setPrice(price);
+                    products[i].setQuantity(quantity);
+                }
             }
         }
-        if(proCount < products.length){
-            products[proCount] = product;
-            proCount++;
-            return "Product added";
-        } else {
-            return "Inventory is full";
-        }
     }
-    public String updateProduct(String name, double price, int quantity){
-        for (int i = 0; i < proCount; i++){
-                products[i].getName().equals(name);
-                products[i].setPrice(price);
-                products[i].setQuantity(quantity);
-                return "Product updated";
-        }
-        return "Invalid Product";
-    }
+
     public String displayAllProduct(){
         String display = "";
+        //find all existing products in the array and add them to the string
         for(int i = 0; i < products.length; i++){
             if (products[i] !=null){
                 display += products[i].getName() + "\n" + products[i].getPrice() + "\n" + products[i].getQuantity() + "\n";
@@ -161,5 +195,4 @@ class Inventory{
         }
         return display;
     }
-
 }
