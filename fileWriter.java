0a64170ca41905fb85e8e83f4bd4a1c5d060dd45
kokoKeremidata <148246518+kokoKeremidata@users.noreply.github.com>
2025-05-05 12:46:15 +0300

    Create fileWriter.java

---
+++ fileWriter.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        for(int i = 0; i >= 0; i++){
+            String in = sc.nextLine();
+            if(in.equals("done")){
+                break;
+            }
+            fileWriter(name,in);
+
+        }
+    }
+    public static void fileWriter(String fileName, String message){
+        String path = "/home/dvangelov26/Informatics/" + fileName + ".txt";
+        try(FileWriter fw = new FileWriter(path, true)){
+            fw.write(message+"\n");
+            System.out.println("write successful");
+        }catch(IOException e){
+            System.out.println("whoops " + e.getMessage());
+        }
+    }
+}
